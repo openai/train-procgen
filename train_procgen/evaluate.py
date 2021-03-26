f1a88bc706317ad1aad240d06671d5023fdad7d2
Eunice Chan <ekchan@berkeley.edu>
2021-03-26 15:23:36 -0700

    start eval script

---
+++ train_procgen/evaluate.py
@@ -0,0 +1,144 @@
+import tensorflow as tf
+from baselines.ppo2 import ppo2
+from baselines.common.models import build_impala_cnn
+from baselines.common.mpi_util import setup_mpi_gpus
+from procgen import ProcgenEnv
+from baselines.common.vec_env import (
+    VecExtractDictObs,
+    VecMonitor,
+    VecFrameStack,
+    VecNormalize
+)
+from baselines import logger
+from mpi4py import MPI
+import argparse
+from .alternate_ppo2 import alt_ppo2
+
+def eval_fn(load_path, log_dir='./tmp/procgen', comm=None):
+#(env_name, num_envs, distribution_mode, num_levels, start_level, timesteps_per_proc, is_test_worker=False, log_dir='./tmp/procgen', comm=None, alternate_ppo=False):
+# load model
+# run on val dataset
+# >> print & log to csv?
+# choose best model based on val results
+# run best model on test
+# >> print & log(?) result
+    learning_rate = 5e-4
+    ent_coef = .01
+    gamma = .999
+    lam = .95
+    nsteps = 256
+    nminibatches = 8
+    ppo_epochs = 3
+    clip_range = .2
+    use_vf_clipping = True
+
+    if log_dir is not None:
+        log_comm = comm.Split(1 if is_test_worker else 0, 0)
+        format_strs = ['csv', 'stdout'] if log_comm.Get_rank() == 0 else []
+        logger.configure(comm=log_comm, dir=log_dir, format_strs=format_strs)
+
+    mpi_rank_weight = 0 if is_test_worker else 1
+    num_levels = 0 if is_test_worker else num_levels
+
+    if log_dir is not None:
+        log_comm = comm.Split(1 if is_test_worker else 0, 0)
+        format_strs = ['csv', 'stdout'] if log_comm.Get_rank() == 0 else []
+        logger.configure(comm=log_comm, dir=log_dir, format_strs=format_strs)
+
+    logger.info("creating environment")
+    venv = ProcgenEnv(num_envs=num_envs, env_name=env_name, num_levels=num_levels, start_level=start_level, distribution_mode=distribution_mode)
+    venv = VecExtractDictObs(venv, "rgb")
+
+    venv = VecMonitor(
+        venv=venv, filename=None, keep_buf=100,
+    )
+
+    venv = VecNormalize(venv=venv, ob=False)
+
+    logger.info("creating tf session")
+    setup_mpi_gpus()
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True #pylint: disable=E1101
+    sess = tf.Session(config=config)
+    sess.__enter__()
+
+    conv_fn = lambda x: build_impala_cnn(x, depths=[16,32,32], emb_size=256)
+
+    logger.info("training")
+    if alternate_ppo:
+        alt_ppo2.learn(
+            env=venv,
+            network=conv_fn,
+            total_timesteps=timesteps_per_proc,
+            load_path=load_path,
+            save_interval=1,
+            nsteps=nsteps,
+            nminibatches=nminibatches,
+            lam=lam,
+            gamma=gamma,
+            noptepochs=ppo_epochs,
+            log_interval=1,
+            ent_coef=ent_coef,
+            mpi_rank_weight=mpi_rank_weight,
+            clip_vf=use_vf_clipping,
+            comm=comm,
+            lr=learning_rate,
+            cliprange=clip_range,
+            update_fn=None,
+            init_fn=None,
+            vf_coef=0.5,
+            max_grad_norm=0.5,
+        )
+    else:
+        ppo2.learn(
+            env=venv,
+            network=conv_fn,
+            total_timesteps=timesteps_per_proc,
+            load_path=load_path,
+            save_interval=1,
+            nsteps=nsteps,
+            nminibatches=nminibatches,
+            lam=lam,
+            gamma=gamma,
+            noptepochs=ppo_epochs,
+            log_interval=1,
+            ent_coef=ent_coef,
+            mpi_rank_weight=mpi_rank_weight,
+            clip_vf=use_vf_clipping,
+            comm=comm,
+            lr=learning_rate,
+            cliprange=clip_range,
+            update_fn=None,
+            init_fn=None,
+            vf_coef=0.5,
+            max_grad_norm=0.5,
+        )
+
+def main():
+    parser = argparse.ArgumentParser(description='Process procgen evaluation arguments.')
+    parser.add_argument('--load_model', type=str, required=True)
+    parser.add_argument('--log_dir', type=str, default='./logs/eval')
+
+    args = parser.parse_args()
+
+    comm = MPI.COMM_WORLD
+
+    eval_fn(args.load_path,
+            log_dir=args.log_dir,
+            comm=comm,
+           )
+    args.env_name,
+        args.num_envs,
+        args.distribution_mode,
+        args.num_levels,
+        args.start_level,
+        args.timesteps_per_proc,
+        is_test_worker=is_test_worker,
+        log_dir=args.log_dir,
+        comm=comm,
+        alternate_ppo=args.alternate_ppo,
+        )
+
+
+if __name__ == '__main__':
+    main()
