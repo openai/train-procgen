f51fd266c02f77697c20d32fb33445b4724f5ee4
eunice-chan <30596854+eunice-chan@users.noreply.github.com>
2021-03-28 11:43:47 -0700

    Update alt_ppo2.py

--- train_procgen/alternate_ppo2/alt_ppo2.py
+++ train_procgen/alternate_ppo2/alt_ppo2.py
@@ -216,31 +216,25 @@ def learn(*, network, env, total_timesteps, eval_env = None, seed=None, nsteps=2
 
     return model
 
-def eval(*, network, env, eval_env = None, seed=None, nsteps=2048, ent_coef=0.0, lr=3e-4,
+def eval(*, network, eval_env = None, seed=None, nsteps=2048, ent_coef=0.0, 
             vf_coef=0.5,  max_grad_norm=0.5, gamma=0.99, lam=0.95,
-            log_interval=10, nminibatches=4, noptepochs=4, cliprange=0.2,
-            save_interval=10, load_path=None, model_fn=None, update_fn=None, init_fn=None, mpi_rank_weight=1, comm=None, **network_kwargs):
+            log_interval=10, nminibatches=4, noptepochs=4, 
+            load_path=None, model_fn=None, update_fn=None, init_fn=None, mpi_rank_weight=1, comm=None, **network_kwargs):
 
     set_global_seeds(seed)
-
-    if isinstance(lr, float): lr = constfn(lr)
-    else: assert callable(lr)
-    if isinstance(cliprange, float): cliprange = constfn(cliprange)
-    else: assert callable(cliprange)
-
-    policy = build_policy(env, network, **network_kwargs)
+ 
+    policy = build_policy(eval_env, network, **network_kwargs)
 
     # Get the nb of env
-    nenvs = env.num_envs
+    nenvs = eval_env.num_envs
 
     # Get state_space and action_space
-    ob_space = env.observation_space
-    ac_space = env.action_space
+    ob_space = eval_env.observation_space
+    ac_space = eval_env.action_space
 
     # Calculate the batch_size
     nbatch = nenvs * nsteps
     nbatch_train = nbatch // nminibatches
-    is_mpi_root = (MPI is None or MPI.COMM_WORLD.Get_rank() == 0)
 
     # Instantiate the model object (that creates act_model and train_model)
     if model_fn is None:
@@ -254,7 +248,7 @@ def eval(*, network, env, eval_env = None, seed=None, nsteps=2048, ent_coef=0.0,
     if load_path is not None:
         model.load(load_path)
     # Instantiate the runner object
-    eval_runner = Runner(env = eval_env, model = model, nsteps = nsteps, gamma = gamma, lam = lam)
+    eval_runner = Runner(env=eval_env, model=model, nsteps=nsteps, gamma=gamma, lam=lam)
 
     eval_epinfobuf = deque(maxlen=100)
 
@@ -264,10 +258,7 @@ def eval(*, network, env, eval_env = None, seed=None, nsteps=2048, ent_coef=0.0,
     # Start total timer
     tfirststart = time.perf_counter()
     
-    # Calculate the cliprange
-    cliprangenow = cliprange(frac)
-
-    if update % log_interval == 0 and is_mpi_root: logger.info('Stepping environment...')
+    logger.info('Stepping environment...')
 
     # Get minibatch
     eval_obs, eval_returns, eval_masks, eval_actions, eval_values, eval_neglogpacs, eval_states, eval_epinfos = eval_runner.run() #pylint: disable=E0632
